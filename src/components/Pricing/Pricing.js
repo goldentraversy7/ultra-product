c76291fa09894415e37f426494fbbdbbf73b0d1f
goldentraversy7 <181262897+goldentraversy7@users.noreply.github.com>
2024-09-22 22:03:13 -0400

    bug fix for deploy - 3rd

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import { Button } from '../../globalStyles';
-import { AiFillThunderbolt } from 'react-icons/ai';
-import { GiCrystalBars } from 'react-icons/gi';
-import { GiCutDiamond, GiRock } from 'react-icons/gi';
-import { GiFloatingCrystal } from 'react-icons/gi';
-import { IconContext } from 'react-icons/lib';
+import React from "react";
+import { Button } from "../../globalStyles";
+import { GiCrystalBars } from "react-icons/gi";
+import { GiCutDiamond, GiRock } from "react-icons/gi";
+import { IconContext } from "react-icons/lib";
 import {
   PricingSection,
   PricingWrapper,
@@ -17,17 +15,17 @@ import {
   PricingCardCost,
   PricingCardLength,
   PricingCardFeatures,
-  PricingCardFeature
-} from './Pricing.elements';
+  PricingCardFeature,
+} from "./Pricing.elements";
 
- const Pricing = () => {
+const Pricing = () => {
   return (
-    <IconContext.Provider value={{ color: '#a9b3c1', size: 64 }}>
+    <IconContext.Provider value={{ color: "#a9b3c1", size: 64 }}>
       <PricingSection>
         <PricingWrapper>
           <PricingHeading>Our Services</PricingHeading>
           <PricingContainer>
-            <PricingCard to='/sign-up'>
+            <PricingCard to="/sign-up">
               <PricingCardInfo>
                 <PricingCardIcon>
                   <GiRock />
@@ -43,7 +41,7 @@ import {
                 <Button primary>Choose Plan</Button>
               </PricingCardInfo>
             </PricingCard>
-            <PricingCard to='/sign-up'>
+            <PricingCard to="/sign-up">
               <PricingCardInfo>
                 <PricingCardIcon>
                   <GiCrystalBars />
@@ -59,7 +57,7 @@ import {
                 <Button primary>Choose Plan</Button>
               </PricingCardInfo>
             </PricingCard>
-            <PricingCard to='/sign-up'>
+            <PricingCard to="/sign-up">
               <PricingCardInfo>
                 <PricingCardIcon>
                   <GiCutDiamond />
@@ -80,6 +78,6 @@ import {
       </PricingSection>
     </IconContext.Provider>
   );
-}
+};
 
-export default Pricing
+export default Pricing;
